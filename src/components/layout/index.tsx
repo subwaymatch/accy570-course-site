22e295119e7cef70682756edbb966d060e721d1f
Ye Joo Park <subwaymatch@gmail.com>
2020-08-22 22:59:47 -0500

    Add favicons

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -31,6 +31,29 @@ export default function Layout({
         <link rel="icon" href="/favicon.ico" />
 
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="apple-mobile-web-app-title" content="ACCY 570" />
+        <meta name="application-name" content="ACCY 570" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#e84a27" />
 
         <script src="https://pyodide-cdn2.iodide.io/v0.15.0/full/pyodide.js"></script>
       </Head>
