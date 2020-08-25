9f8cd4bca60af9c60d281d04ed2116292a2bbc1e
Ye Joo Park <subwaymatch@gmail.com>
2020-08-25 07:33:26 -0500

    Responsive layouts

--- src/pages/schedule.tsx
+++ src/pages/schedule.tsx
@@ -1,10 +1,6 @@
 import { motion } from 'framer-motion';
 import Layout from 'src/components/layout';
-import styles from './schedule.module.scss';
-import classNames from 'classnames/bind';
 import CourseCalendar from 'src/components/course-calendar';
-
-const cx = classNames.bind(styles);
 
 export default function SchedulePage() {
   return (
