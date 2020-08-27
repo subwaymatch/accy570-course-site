50337a826f2f05f7679197a63ea5762c32545b11
Ye Joo Park <subwaymatch@gmail.com>
2020-08-26 20:51:13 -0500

    Fix incorrect solution code in deloitte-vs-pwc-01

--- content/question/python-coding/deloitte-vs-pwc-01/solution.py
+++ content/question/python-coding/deloitte-vs-pwc-01/solution.py
@@ -2,7 +2,7 @@ deloitte_offer = 81000
 pwc_offer = 85000
 
 # Your code below this line
-if ...:
+if deloitte_offer > pwc_offer:
     print('Accept Deloitte offer')
 else:
     print('Accept PwC offer')
