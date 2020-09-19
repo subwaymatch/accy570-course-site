aa733a00015a0a8ede03256644c4d73029720923
Ye Joo Park <subwaymatch@gmail.com>
2020-09-19 09:00:09 -0500

    Work-in-progress new dashboard

---
+++ components/live-dashboard/index.tsx
@@ -0,0 +1,10 @@
+import styles from './live-dashboard.module.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
+
+type LiveDashboardProps = {};
+
+export default function LiveDashboard({}: LiveDashboardProps) {
+  return <div>Dashboard Preview</div>;
+}
---
+++ pages/live/dashboard-preview.tsx
@@ -0,0 +1,10 @@
+import Layout from 'components/layout';
+import LiveDashboard from 'components/live-dashboard';
+
+export default function LiveDashboardPage() {
+  return (
+    <Layout fluid>
+      <LiveDashboard />
+    </Layout>
+  );
+}
